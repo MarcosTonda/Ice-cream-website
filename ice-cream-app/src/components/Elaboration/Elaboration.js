9a53a77baf822c0a18fc7719cfdadec78a95ea6c
Marcos <marcostonda@gmail.com>
2022-05-12 18:05:35 -0300

    Se acomodan estilos de fotografias

--- ice-cream-app/src/components/Elaboration/Elaboration.js
+++ ice-cream-app/src/components/Elaboration/Elaboration.js
@@ -7,35 +7,33 @@ import tilde from './Images/tilde.png'
 const Elaboration = () => {
   return (
 
-    <div className='container md:px-5 py-5 h-auto grid grid-row-2 max-w-full
-                    lg:grid lg:grid-cols-2 lg:gap-x-5'>
+    <div className='container grid h-auto max-w-full py-5 md:px-5 grid-row-2 lg:grid lg:grid-cols-2 lg:gap-x-5'>
 
-        <div className='container-text flex flex-col gap-y-5 mx-2 md:mx-0'>        
+        <div className='flex flex-col mx-2 container-text gap-y-5 md:mx-0'>        
 
-          <div className='container-title flex flex-wrap text-xl font-bold
-                          md:text-2xl xl:text-3xl'>
+          <div className='flex flex-wrap text-xl font-bold container-title md:text-2xl xl:text-3xl'>
 
-            <p className='text-green-700 pr-2 pb-7'>Elaboracion de</p><p>Nuestros Productos</p>
+            <p className='pr-2 text-green-700 pb-7'>Elaboracion de</p><p>Nuestros Productos</p>
             
           </div>          
 
-          <div className='containter-text  flex flex-wrap gap-y-6 mb-3'>
+          <div className='flex flex-wrap mb-3 containter-text gap-y-6'>
 
             <div className='flex flex-nowrap '>
 
-            <img className='w-7 h-7 pr-2 lg:w-10 lg:h-10' src={tilde} alt="tilde" /><p>Nuestra misión es hacerte feliz a ti y ayudar a nuestra comunidad.  De qué manera?</p>               
+            <img className='pr-2 w-7 h-7 lg:w-10 lg:h-10' src={tilde} alt="tilde" /><p>Nuestra misión es hacerte feliz a ti y ayudar a nuestra comunidad.  De qué manera?</p>               
 
             </div>  
 
             <div className='flex flex-nowrap '>
 
-              <img className='w-7 h-7 pr-2 lg:w-10 lg:h-10' src={tilde} alt="tilde" /><p>Haciendo helados que disfrutarás comer como ninguna otra cosa en el mundo.</p>                       
+              <img className='pr-2 w-7 h-7 lg:w-10 lg:h-10' src={tilde} alt="tilde" /><p>Haciendo helados que disfrutarás comer como ninguna otra cosa en el mundo.</p>                       
 
             </div>        
 
             <div className='flex flex-nowrap '>
               
-              <img className='w-7 h-7 pr-2 lg:w-10 lg:h-10' src={tilde} alt="tilde"/><p>Creando sabores únicos que soprenden a todas las personas que los prueban.</p>            
+              <img className='pr-2 w-7 h-7 lg:w-10 lg:h-10' src={tilde} alt="tilde"/><p>Creando sabores únicos que soprenden a todas las personas que los prueban.</p>            
 
             </div>
          
@@ -47,7 +45,7 @@ const Elaboration = () => {
 
             <div className='flex flex-nowrap'>
               
-              <img className='w-7 h-7 pr-2 lg:w-10 lg:h-10' src={tilde} alt="tilde"/><p>Siendo inclusivos al crear alternativas para todos, desde personas con intolerancias alimentarias hasta que por cuestiones éticas no desean comer productos hechos con componentes animales.</p>            
+              <img className='pr-2 w-7 h-7 lg:w-10 lg:h-10' src={tilde} alt="tilde"/><p>Siendo inclusivos al crear alternativas para todos, desde personas con intolerancias alimentarias hasta que por cuestiones éticas no desean comer productos hechos con componentes animales.</p>            
 
             </div>
 
@@ -55,7 +53,7 @@ const Elaboration = () => {
 
           <div>
 
-            <button className='bg-blue-700 mt-3 p-3 mb-3 text-white border-2 border-none font-extrabold rounded-xl hover:bg-purple-600'>Descubre Nuestros Productos</button>
+            <button className='p-3 mt-3 mb-3 font-extrabold text-white bg-blue-700 border-2 border-none rounded-xl hover:bg-purple-600'>Descubre Nuestros Productos</button>
 
           </div>
 
@@ -63,7 +61,7 @@ const Elaboration = () => {
 
         <div className='container-image'>
 
-          <img className='w-full h-full'src={elaboration} alt="Elaboracion de Nuestros Productos" />
+          <img className='object-cover object-center w-11/12 h-5/6'src={elaboration} alt="Elaboracion de Nuestros Productos" />
 
         </div>        
 
